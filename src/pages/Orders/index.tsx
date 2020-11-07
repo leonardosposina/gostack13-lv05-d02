a815612b5f331af8429900727db612b5d77d90e2
Leonardo Sposina <leonardo.sposina@icloud.com>
2020-11-07 01:17:40 -0300

    feat: List food orders on Orders page

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -32,7 +32,15 @@ const Orders: React.FC = () => {
 
   useEffect(() => {
     async function loadOrders(): Promise<void> {
-      // Load orders from API
+      const response = await api.get<Food[]>('/orders');
+
+      const updatedOrders = response.data.map(food =>
+        Object.assign(food, {
+          formattedPrice: formatValue(food.price),
+        }),
+      );
+
+      setOrders(updatedOrders);
     }
 
     loadOrders();
