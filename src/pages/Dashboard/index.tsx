01730d6d7051f7d0dd4dba7fcafde9d5b48253c8
Leonardo Sposina <leonardo.sposina@icloud.com>
2020-11-07 00:33:55 -0300

    feat: List food filtered by the query params

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -59,7 +59,12 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      const response = await api.get('/foods');
+      const response = await api.get('/foods', {
+        params: {
+          category_like: selectedCategory,
+          name_like: searchValue,
+        },
+      });
       setFoods(response.data);
     }
 
